fc8ac004850cd8d62780a1a7a5e8f4ff0c54690f
Simó Fanna <simofanna93@gmail.com>
2017-06-23 17:26:23 +0200

    Update gameInventory.py

--- gameInventory.py
+++ gameInventory.py
@@ -3,6 +3,9 @@
 
 # Displays the inventory.
 def display_inventory(inventory):
+    inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+    for item in inventory:
+    
     pass
 
 
